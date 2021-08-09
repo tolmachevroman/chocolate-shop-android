10af9d5b164ca44280a6914e6ce0001823222612
tolmachevroman <tolmachevroman@gmail.com>
2021-08-09 12:11:43 +0400

    show Product data in ChocolateDetailsActivity

--- app/src/main/java/com/chocolate/shop/views/activities/ChocolateDetailsActivity.kt
+++ app/src/main/java/com/chocolate/shop/views/activities/ChocolateDetailsActivity.kt
@@ -6,8 +6,10 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.chocolate.shop.GetProductQuery
 import com.chocolate.shop.databinding.ActivityChocolateDetailsBinding
+import com.chocolate.shop.type.ChocolateType
 import com.chocolate.shop.utils.ResourceObserver
 import com.chocolate.shop.viewmodels.ChocolateDetailsViewModel
+import com.squareup.picasso.Picasso
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -43,7 +45,36 @@ class ChocolateDetailsActivity : AppCompatActivity() {
     }
 
     private fun onSuccess(data: GetProductQuery.Product?) {
+        data?.apply {
+            Picasso.get()
+                .load(images[0])
+                .into(binding.image);
+            binding.name.text = name
+            binding.description.text = description
+            binding.chocolateType.text = formatChocolateType(chocolateType)
+            binding.fillings.text = formatFillings(fillings)
+            binding.price.text = formatPrice(price)
+        }
+    }
 
+    private fun formatChocolateType(chocolateType: ChocolateType): String {
+        return chocolateType.toString().lowercase()
+            .replaceFirstChar { it.uppercase() } + " chocolate"
+    }
+
+    private fun formatFillings(fillings: List<String>): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("Contains ")
+        for ((i, f) in fillings.withIndex()) {
+            stringBuilder.append(f.lowercase())
+            if (i < fillings.size - 1) stringBuilder.append(", ")
+        }
+
+        return stringBuilder.toString()
+    }
+
+    private fun formatPrice(price: Int): String {
+        return "$$price per kilo"
     }
 
     private fun onError(error: String?) {}
